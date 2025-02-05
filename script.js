157c164060d85d50be8660819a24bc001e8ed6c6
Rudra Patel <work.rudra@aol.com>
2025-02-05 20:55:41 +0000

    Add functions in script.js to get machine's choice for the game
    Write 2 functions which randomly decides machine's outcome of rock, paper, and scissor. The first function randomly selects an integer from 0, 1, and 2. The second function makes use of the first function by using the randomly selected ineteger to decide the outcome.

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 console.log("Hello, World!");
+
+// Choose a random integer which is greater than or equal to 0 and less than 3 (i.e., return random integer from 0, 1, and 2)
+function chooseRandomInt() {
+  let randomInt = Math.floor(Math.random() * 3);
+  return randomInt;
+}
+
+// Randomly return Rock, Paper or Scissor as the machine's choice
+function getMachineChoice() {
+  let choice = chooseRandomInt();
+
+  if (choice === 0) {
+    console.log("Rock");
+  } else if (choice === 1) {
+    console.log("Paper");
+  } else {
+    // when chooseRandomInt() returns 2
+    console.log("Scissor");
+  }
+}
+
+getMachineChoice();
