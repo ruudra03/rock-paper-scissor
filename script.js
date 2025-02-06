80a3673ceb6d2444b9caa9ade78068f23cf9348a
Rudra Patel <work.rudra@aol.com>
2025-02-06 19:30:44 +0000

    Add a function in script.js to get user's choice for the game
    The function accepts integer values as user's choice and returns corresponding outcome of Rock, Paper or Scissor. It logs a message when user chooses an integer outside the given set of valid options.

--- script.js
+++ script.js
@@ -21,3 +21,25 @@ function getMachineChoice() {
 }
 
 getMachineChoice();
+
+// Get user's choice and return Rock, Paper or Scissor as outcome
+function getUserChoice() {
+  let choice = parseInt(
+    prompt(
+      "Please enter your choice:\n(Enter 1 for 'Rock', 2 for 'Paper', or 3 for 'Scissor')"
+    )
+  );
+
+  if (choice === 1) {
+    console.log("Rock");
+  } else if (choice === 2) {
+    console.log("Paper");
+  } else if (choice === 3) {
+    console.log("Scissor");
+  } else {
+    // when integer of choice is not from 1, 2 or 3
+    console.log("Invalid choice, please choose from the given options.");
+  }
+}
+
+getUserChoice();
