96efed293615248fb415f0cde3ab3484b93fd86c
Rudra Patel <work.rudra@aol.com>
2025-02-06 20:03:21 +0000

    Add a function in script.js which stimulates a round played
    The function playRound accepts user's and machine's outcomes based on simultaneous choices and decides the round's result based on game's rules. The function also increments the points of the winner utilising relevant counters.

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 console.log("Hello, World!");
 
+// Game Counters
+let machinePoints = 0;
+let userPoints = 0;
+
 // Choose a random integer which is greater than or equal to 0 and less than 3 (i.e., return random integer from 0, 1, and 2)
 function chooseRandomInt() {
   let randomInt = Math.floor(Math.random() * 3);
@@ -9,18 +13,19 @@ function chooseRandomInt() {
 // Randomly return Rock, Paper or Scissor as the machine's choice
 function getMachineChoice() {
   let choice = chooseRandomInt();
+  let outcome;
 
   if (choice === 0) {
-    console.log("Rock");
+    outcome = "Rock";
   } else if (choice === 1) {
-    console.log("Paper");
+    outcome = "Paper";
   } else {
     // when chooseRandomInt() returns 2
-    console.log("Scissor");
+    outcome = "Scissor";
   }
-}
 
-getMachineChoice();
+  return outcome;
+}
 
 // Get user's choice and return Rock, Paper or Scissor as outcome
 function getUserChoice() {
@@ -29,17 +34,44 @@ function getUserChoice() {
       "Please enter your choice:\n(Enter 1 for 'Rock', 2 for 'Paper', or 3 for 'Scissor')"
     )
   );
+  let outcome;
 
   if (choice === 1) {
-    console.log("Rock");
+    outcome = "Rock";
   } else if (choice === 2) {
-    console.log("Paper");
+    outcome = "Paper";
   } else if (choice === 3) {
-    console.log("Scissor");
+    outcome = "Scissor";
   } else {
-    // when integer of choice is not from 1, 2 or 3
-    console.log("Invalid choice, please choose from the given options.");
+    // when integer of choice is NOT from valid options 1, 2 or 3
+    outcome = null;
   }
+
+  return outcome;
 }
 
-getUserChoice();
+// Play a round using machine's and user's outcomes
+function playRound(userOutcome, machineOutcome) {
+  let result;
+
+  if (userOutcome === null) {
+    result = "Invalid outcome!";
+  } else if (userOutcome === machineOutcome) {
+    result = `Draw! ${userOutcome} vs ${machineOutcome}.`;
+  } else if (
+    (userOutcome === "Rock" && machineOutcome === "Paper") ||
+    (userOutcome === "Paper" && machineOutcome === "Scissor") ||
+    (userOutcome === "Scissor" && machineOutcome === "Rock")
+  ) {
+    machinePoints++;
+    result = `Loss! ${userOutcome} is defeated by ${machineOutcome}.`;
+  } else {
+    // when the conditional "(userOutcome === "Rock" && machineOutcome === "Scissor") || (userOutcome === "Paper" && machineOutcome === "Rock") || (userOutcome === "Scissor" && machineOutcome === "Paper")" is true
+    userPoints++;
+    result = `Win! ${userOutcome} defeats ${machineOutcome}.`;
+  }
+
+  return result;
+}
+
+console.log(playRound(getUserChoice(), getMachineChoice()));
